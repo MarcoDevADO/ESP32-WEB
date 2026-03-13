a14f9bd22905c9b351a241ca3d724816d442df27
MarcoDevADO <marcopalgm@gmail.com>
2026-03-12 19:05:44 -0600

    modificacion

--- ‎static/script.js
+++ ‎static/script.js
@@ -67,7 +67,7 @@ async function updateData() {
             row.insertCell(0).innerText = dato.ax;
             row.insertCell(1).innerText = dato.ay;
             row.insertCell(2).innerText = dato.az;
-            row.insertCell(3).innerText = dato.emg || "-";
+            row.insertCell(3).innerText = dato.emg;
         });
 
     } catch (error) {
